ff60528e35a96111cdc73b059e1144206388580e
ujwaladhikari <adhikaridujwal001@gmail.com>
2024-01-07 01:02:57 +0545

    Login page frontend

--- lib/Screens/OtherScreens/splash_screen.dart
+++ lib/Screens/OtherScreens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kidspace/Screens/OtherScreens/login_screen.dart';
 import 'package:kidspace/Widgets/bottom_bar.dart';
 import 'package:kidspace/blocs/internet_bloc/internet_bloc.dart';
 import 'package:kidspace/blocs/internet_bloc/internet_state.dart';
@@ -12,27 +13,31 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: BlocBuilder<InternetBloc, InternetState>(
-            builder: (context, state) {
-              if (state is InternetGainedState) {
-                return const CustomButtomBar();
-              } else {
-                return AlertDialog(
-                  title: const Text('Network Problem'),
-                  content: const Text("Internet connection not available!"),
-                  actionsAlignment: MainAxisAlignment.spaceBetween,
-                  actions: <Widget>[
-                    const CircularProgressIndicator(),
-                    TextButton(
-                        onPressed: () {
-                          SystemNavigator.pop();
-                        },
-                        child: const Text("Exit")),
-                  ],
-                );
-              }
-            },
+        child: Container(
+          decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/background/Splash screen.png"),fit: BoxFit.cover)),
+          child: Center(
+            child: BlocBuilder<InternetBloc, InternetState>(
+              builder: (context, state) {
+                if (state is InternetGainedState) {
+                  return const LoginScreen();
+                } else {
+                  return AlertDialog(
+                    alignment: AlignmentDirectional.bottomEnd,
+                    title: const Text('Network Problem'),
+                    content: const Text("Internet connection not available!"),
+                    actionsAlignment: MainAxisAlignment.spaceBetween,
+                    actions: <Widget>[
+                      const CircularProgressIndicator(),
+                      TextButton(
+                          onPressed: () {
+                            SystemNavigator.pop();
+                          },
+                          child: const Text("Exit")),
+                    ],
+                  );
+                }
+              },
+            ),
           ),
         ),
       ),
